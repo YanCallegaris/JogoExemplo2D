4e0ccde5f5012fe51c5c87851e2a2eae4409f4ce
Yan <callegaris.yan@gmail.com>
2025-12-15 00:32:22 -0300

    Yan - movement player part 1

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
@@ -12,9 +13,31 @@ public class PlayerController : MonoBehaviour
 
 	void Update()
 	{
+		float horizontal = 0f;
+		float vertical = 0f;
+		if (Keyboard.current.leftArrowKey.isPressed)
+		{
+			horizontal = -1f;
+		}
+		else if (Keyboard.current.rightArrowKey.isPressed)
+		{
+			horizontal = 1f;
+		}
+
+		if (Keyboard.current.downArrowKey.isPressed)
+		{
+			vertical = -1f;
+		}
+		else if (Keyboard.current.upArrowKey.isPressed)
+		{
+			vertical = 1f;
+		}
+
+		Debug.Log(horizontal);
+
 		Vector2 position = transform.position;
-		position.y = position.y + speed;
-		position.x = position.x + speed;
+		position.x = position.x + speed * horizontal;
+		position.y = position.y + speed * vertical;
 		transform.position = position;
 	}
 }
