e106bc3d001066a0fccfa4030939643bc5e80e67
Yan <callegaris.yan@gmail.com>
2025-12-26 21:07:31 -0300

    Yan - healthy system

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -5,18 +5,34 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+	public int maxHealth = 5;
+	int currentHealth;
 	public float speed;
 	public InputAction moveAction;
+	Rigidbody2D rigidbody2D;
+	Vector2 move;
 	void Start()
 	{
 		moveAction.Enable();
+		rigidbody2D = GetComponent<Rigidbody2D>();
 	}
 
 	void Update()
 	{
-		Vector2 move = moveAction.ReadValue<Vector2>();
+		currentHealth = maxHealth;
+		move = moveAction.ReadValue<Vector2>();
 		Debug.Log(move);
+	}
+
+    private void FixedUpdate()
+    {
 		Vector2 position = (Vector2)transform.position + move * speed * Time.deltaTime;
 		transform.position = position;
+	}
+
+	void ChangeHealth(int amount)
+	{
+		currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+		Debug.Log(currentHealth + "/" + maxHealth);
 	}
 }
